85f6b70835f8720e047dc2bafb88cd5b49d1db14
MoisesRM32 <141669746+MoisesRM32@users.noreply.github.com>
2026-07-14 19:55:27 -0700

    fixed some errors involing javascript

--- js/script.js
+++ js/script.js
@@ -1,16 +1,2 @@
 console.log("Portfolio loaded successfully.");
 
-
-const projectDialog = document.querySelector("#project-dialog");
-const openDialogButton = document.querySelector("#open-project-dialog");
-const closeDialogButton = document.querySelector("#close-project-dialog");
-
-if (projectDialog && openDialogButton && closeDialogButton) {
-    openDialogButton.addEventListener("click", () => {
-        projectDialog.showModal();
-    });
-
-    closeDialogButton.addEventListener("click", () => {
-        projectDialog.close();
-    });
-}
